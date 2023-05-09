0f33c74a622b251783bdf37fef0b02bf0c20974f
Ameya Kherodkar <ameyack@gmail.com>
2023-05-09 00:08:01 -0400

    First Commit

---
+++ SuperHero-ng/SuperHero.UI/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production : false,
+    apiUrl : "https://localhost:7229/api",
+
+};
---
+++ SuperHeroNG/SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public SuperHeroController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<SuperHero>>> getSuperHeros()
+        {
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            _dataContext.SuperHeroes.Add(hero);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbHero = await _dataContext.SuperHeroes.FindAsync(hero.Id);
+            if(dbHero == null)
+            {
+                return BadRequest("Hero Not Found");
+            }
+
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _dataContext.SaveChangesAsync();
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbHero = await _dataContext.SuperHeroes.FindAsync(id);
+            if(dbHero == null)
+            {
+                return BadRequest("Hero Not Found");
+            }
+
+            _dataContext.SuperHeroes.Remove(dbHero);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+
+        }
+        
+    }
+}
